99172eaae6b7292e1946719e55f21b31a597a5bc
James Chetwood <james@jameschetwood.co.uk>
2021-10-14 15:05:35 +0100

    WIP

--- src/App.js
+++ src/App.js
@@ -4,20 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{ minHeight: 300, background: 'gold' }}>
+        <i className="fa-solid fa-sensor"></i>
+        <i className="fa-regular fa-sensor"></i>
+      </div>
     </div>
   );
 }
